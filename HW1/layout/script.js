401f21ec4e704a9d554b64ed76d9260b9d05cf02
ShlomiFridman <shlomif99@gmail.com>
2024-11-29 10:51:58 +0200

    added base files

--- HW1/layout/script.js
+++
@@ -1,4 +0,0 @@
-function login() {
-    // No username or password validation
-    window.location.href = "home.html";
-}
---
+++ Prototype/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'light_BgColor' : 'antiquewhite',
+      'dark_BgColor' : "#2f4f4f"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
